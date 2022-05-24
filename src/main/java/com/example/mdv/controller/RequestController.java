c2d5f013cba7186de87136816d4a6a22a9bb9ba1
denmaltsev <denmaltsev@gmail.com>
2022-05-24 11:31:52 +1000

    Update RequestController.java
    get response data from process

--- src/main/java/com/example/mdv/controller/RequestController.java
+++ src/main/java/com/example/mdv/controller/RequestController.java
@@ -15,7 +15,12 @@ public class RequestController {
 
     //todo: перенести константы в отдельный класс
     private final static String INPUT_DATA="intutData";
-    private final static String SCENARIO_ID="Scenatio name put here"
+    private final static String SCENARIO_ID="Scenatio name put here";
+    
+    public static final String DEFAULT_ERROR_RESPONSE_BODY = "{ \"status\": \"error\" }";
+    public static final String BPMN_PROCESS_RESPONSE = "bpmnProcessResponse";
+    public static final String BPMN_PROCESS_STATUS = "bpmnProcessStatus";
+    public static final String HTTP_RESPONSE_STATUS = "httpResponseStatus";
     
     @PostMapping(value = "/run", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> run(@RequestBody String request) {
@@ -27,8 +32,27 @@ public class RequestController {
         log.info("Received request: {}", request);
         
         Map<String, Object> bpmnProcessExecutoionResultMap = startProcess(SCENARIO_ID, UUID.randomUUID().toString(), scenatioParamsMap);
-            
-        return new ResponseEntity<>("ok", HttpStatus.OK);
+
+        String bpmnProcessWrappedResponse = getBpmnExecutionVariableValue(bpmnProcessExecutionResultMap, TransactSmScenarioVariables.BPMN_PROCESS_RESPONSE);
+        String bpmnProcessStatus = getBpmnExecutionVariableValue(bpmnProcessExecutionResultMap, BPMN_PROCESS_STATUS);
+        String bpmnResponseHttpCode = getBpmnExecutionVariableValue(bpmnProcessExecutionResultMap, HTTP_RESPONSE_STATUS);
+
+        String bpmnProcessResponse = getMessageFromBpmnWrapper(bpmnProcessWrappedResponse);
+
+        final Optional<HttpStatus> status = Optional.of(resolveProcessResponseHttpCode(bpmnResponseHttpCode, bpmnProcessStatus));
+        String response = StringUtils.isNotBlank(bpmnProcessResponse) ? bpmnProcessResponse : DEFAULT_ERROR_RESPONSE_BODY;
+        
+        log.info("Sent response: {}", response);
+        log.debug("Sent response with httpCode='{}'", status.get().value());
+
+        return ResponseEntity
+                .status(status.get())
+                .headers(headers)
+                .headers(headersOut)
+                .contentType(APPLICATION_JSON)
+                .contentLength(response.getBytes().length)
+                .body(response);        
+    //    return new ResponseEntity<>("ok", HttpStatus.OK);
     }
 
        private Map<String, Object> startProcess(@NotNull String scenarioId, @NotNull String businessKey, Map<String, Object> payload) {
@@ -63,4 +87,47 @@ public class RequestController {
         }
         return null;
     }
+    
+    private HttpStatus resolveProcessResponseHttpCode(String bpmnResponseHttpCode, String bpmnProcessStatus) {
+        HttpStatus status = null;
+        if (StringUtils.isNotBlank(bpmnResponseHttpCode)) {
+            try {
+                status = HttpStatus.resolve(Integer.parseInt(bpmnResponseHttpCode));
+            } catch (NumberFormatException ex) {
+                log.warn("Переменная bpmnResponseHttpCode содержит значение, не являющееся целым числом: {}", bpmnResponseHttpCode);
+            }
+        }
+        if (status == null && StringUtils.isNotBlank(bpmnProcessStatus)) {
+            status = SUCCESS.toString().equals(bpmnProcessStatus) ? DEFAULT_RESPONSE_HTTP_SUCCESS_STATUS :
+                    DEFAULT_RESPONSE_HTTP_ERROR_STATUS;
+
+        }
+        return (status != null) ? status : DEFAULT_RESPONSE_HTTP_ERROR_STATUS;
+    }
+    
+    private String getBpmnExecutionVariableValue(Map<String, Object> bpmnProcessExecutionResultMap, String variableName) {
+        String bpmnExecutionVariableValue = null;
+        final List<Map<String, Object>> resultVariables = (List<Map<String, Object>>) bpmnProcessExecutionResultMap.get("variables");
+        final Optional<Map<String, Object>> resultVarMap =
+                resultVariables.stream()
+                        .filter(varMap -> varMap.get("name").equals(variableName))
+                        .findFirst();
+        if (resultVarMap.isPresent()) {
+            bpmnExecutionVariableValue = resultVarMap.get().get("value").toString();
+        }
+        return bpmnExecutionVariableValue;
+    }
+
+    private String getMessageFromBpmnWrapper(String wrappedMessage) {
+        final String MESSAGE_PATH = "message";
+
+        String message = wrappedMessage;
+        try {
+            //todo: change to Jackson
+            message = GSON.fromJson(message, JsonObject.class).get(MESSAGE_PATH).getAsString();
+        } catch (Exception ex) {
+            log.warn("Cannot get message from a wrapper response!");
+        }
+        return message;
+    }    
 }
